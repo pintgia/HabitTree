9a94a43c3b6b3b0ff39345025221b81be4225a3f
Gian Pinto <gianrobertpinto@gmail.com>
2021-04-09 12:16:49 -0500

    Main Feature Demo

--- Habit Tree/ViewController.swift
+++ Habit Tree/ViewController.swift
@@ -8,15 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    let progress = Progress(totalUnitCount: 5)
+    
+    
+    
+    @IBAction func buttonCompleteLogPressed(_ sender: Any) {
+        self.progress.completedUnitCount += 1
+        
+        let progressFloat = Float(self.progress.fractionCompleted)
+        self.progressView.setProgress(progressFloat, animated: true)
+        
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view.
-        print("Habit Tree")
-        print("Hello World")
-        print("Hello Again 2")
+       
     }
 
 
 }
-
