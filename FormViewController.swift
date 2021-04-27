96c3b30d058bac3cbcf2010c5310afb701a3b07c
daniel hernandez <hdaniel511@yahoo.com>
2021-04-26 19:57:17 -0500

    log function

--- FormViewController.swift
+++ FormViewController.swift
@@ -8,8 +8,15 @@ import UIKit
 
 class FormViewController: UIViewController {
 
+    @IBOutlet var segmentedControl: UISegmentedControl!
     @IBOutlet weak var saveButton: UIButton!
- 
+    @IBOutlet var newTreeTextField: UITextField!
+    @IBOutlet var newTreeLabel2: UILabel!
+    @IBOutlet var newTreeLabel1: UILabel!
+    @IBOutlet var logActionTextField: UITextField!
+    @IBOutlet var logActionLabel2: UILabel!
+    @IBOutlet var logActionLabel1: UILabel!
+    
     let treeName = "Do Chores"
     let action: [ String: Bool ] = [
         "Take out trash": true
@@ -18,6 +25,11 @@ class FormViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        newTreeLabel1.isHidden = true
+        newTreeLabel2.isHidden = true
+        newTreeTextField.isHidden = true
+        
         view.backgroundColor = .systemYellow
         loadData()
     }
@@ -33,4 +45,39 @@ class FormViewController: UIViewController {
         DataManager.shared.createData(new: treeName, of: action)
     }
 
+    @IBAction func segmentedControlValueChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            logActionLabel1.isHidden = false
+            logActionLabel2.isHidden = false
+            logActionTextField.isHidden = false
+            newTreeLabel1.isHidden = true
+            newTreeLabel2.isHidden = true
+            newTreeTextField.isHidden = true
+        }
+        else {
+            logActionLabel1.isHidden = true
+            logActionLabel2.isHidden = true
+            logActionTextField.isHidden = true
+            newTreeLabel1.isHidden = false
+            newTreeLabel2.isHidden = false
+            newTreeTextField.isHidden = false
+        }
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let dvc = segue.destination as! HomeViewController
+        if segmentedControl.selectedSegmentIndex == 0{
+            if let logActionText = logActionTextField.text {
+                dvc.userInput = logActionText
+            }
+            
+            
+        } else {
+            if let newTreeText = newTreeTextField.text {
+                dvc.userInput = newTreeText
+            }
+        }
+        
+        
+    }
 }
--- HomeViewController.swift
+++ HomeViewController.swift
@@ -8,14 +8,19 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet var textLabel: UILabel!
+    var userInput = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemPurple
-
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func unwindToHome(_ unwindSegue: UIStoryboardSegue) {
+        textLabel.text = userInput
+        
+        // Use data from the view controller which initiated the unwind segue
+    }
 }
